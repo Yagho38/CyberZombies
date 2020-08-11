480821160a12cab96a2ed3250bd58c1015a3517e
yagho38 <yaghs39@gmail.com>
2020-08-11 18:52:30 -0300

    version 1.1.1.1

---
+++ Autostart comoarrumar.py
@@ -0,0 +1,16 @@
+from os import path, chdir, listdir, getcwd, system
+from zipfile import ZipFile
+import sys
+
+workingPath = getcwd()
+system("sudo apt-get install crontab")
+print("\n\n\n\n\nProgram Starting Now:\n\n")
+system("sudo chmod 777 /home/logs/launcher.sh")
+system("sudo chmod 777 /etc/init.d")
+system("sh /home/logs/launcher.sh")
+
+#adicionar ao seu python
+cd /
+cd /home/logs/#script python
+sudo #script python
+cd /
